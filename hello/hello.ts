ffc3de258885e6c8a78efc1f4e33777f1585ae03
xavier brinon <xavierbrinon@xaviers-MacBook-Air.local>
2024-09-06 11:29:24 +0100

    feat: first import

--- hello/hello.ts
+++ hello/hello.ts
@@ -15,8 +15,8 @@ import { api } from "encore.dev/api";
 export const get = api(
   { expose: true, method: "GET", path: "/hello/:name" },
   async ({ name }: { name: string }): Promise<Response> => {
-    const msg = `Hello ${name}!`;
-    return { message: msg };
+    const message = `Hello ${name}!`;
+    return { message };
   }
 );
 
